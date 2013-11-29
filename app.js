a562a1a0b8f96c78ee3dc3e99fc28abb7644bd39
Peter Kinnaird <peter@peterkinnaird.eu>
2013-11-29 22:19:43 +0000

    update map to have data points

--- app.js
+++ app.js
@@ -147,7 +147,14 @@ app.put('/people/:id', function (req, res) {
 app.get('/map', function (req, res) {  
 res.render('dashboards/map', {
     page : 'Map',
-    mapelement : 'map'
+    mapelement : 'map',
+    datapoints: [
+      {latitude: 55.8506080519, longitude: -4.26492576329, weight: 100},
+        {latitude: 55.8613701277, longitude: -4.24460130892, weight: 100},
+        {latitude: 55.4906916873, longitude: -4.60522901822, weight: 100},
+        {latitude: 55.8651840586, longitude: -4.2216469545 , weight: 100}
+      ]
+    
   });
 });
 
