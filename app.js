d54df18a1de1aba92969d3c341d89ad4df2b3fc0
Robert Wilson <robert.wilson1717@gmail.com>
2013-11-30 02:29:03 +0000

    UpdatePerson

--- app.js
+++ app.js
@@ -87,8 +87,8 @@ res.render('account', {
 
 app.post('/account',function (req, res) {
 //req.body.facebookId = req.user.facebookId;
-  //data.updatePerson(req.body);
-  console.log(JSON.stringify(req.body));
+  data.updatePerson(req.user.facebookIdreq.body);
+  //console.log(JSON.stringify(req.body));
   res.statusCode = 200;
   return res.send('Inserted');
   });
